3264790a3e2c161f88aaba9ddb7cc56822768065
Michael Bridgen <mikeb@squaremobius.net>
2014-01-10 02:01:32 +1300

    Add reader-expand for seeing reader macros in action
    pp changes to not quote not quoted things (ops) and to have optional
    shortening (of quoted things).

--- pp.erl
+++ pp.erl
@@ -1,23 +1,28 @@
 -module(pp).
 
--export([pp/1]).
+-export([pp/1, pp/2]).
 
-pp(Bin) when is_binary(Bin) ->
+pp(Val) -> pp(Val, false).
+
+%% Val, UseContractions
+pp(Bin, _) when is_binary(Bin) ->
     [$", binary_to_list(Bin), $"];
-pp(true) -> "#t";
-pp(false) -> "#f";
-pp(Atom) when is_atom(Atom) ->
-    [$', atom_to_list(Atom)];
-pp([]) ->
+pp(true, _) -> "#t";
+pp(false, _) -> "#f";
+pp(Atom, _) when is_atom(Atom) ->
+    atom_to_list(Atom);
+pp([], _) ->
     "()";
-pp([Head | Tail]) ->
-    ["(", pp(Head), pp_rest(Tail)];
-pp(Num) when is_integer(Num) ->
+pp([quote, Value], true) ->
+    [$', pp(Value)];
+pp([Head | Tail], C) ->
+    ["(", pp(Head, C), pp_rest(Tail, C)];
+pp(Num, _) when is_integer(Num) ->
     integer_to_list(Num);
-pp(Num) when is_float(Num) ->
+pp(Num, _) when is_float(Num) ->
     float_to_list(Num).
 
-pp_rest([]) ->
+pp_rest([], _) ->
     ")";
-pp_rest([Head | Tail]) ->
-    [" ", pp(Head), pp_rest(Tail)].
+pp_rest([Head | Tail], C) ->
+    [" ", pp(Head, C), pp_rest(Tail, C)].
